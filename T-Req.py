852428b4e921cdfe1a18680f7ab925c3cab18a49
John Kener <73270248+John-kener@users.noreply.github.com>
2021-06-28 20:07:58 +0530

    Add files via upload

---
+++ T-Req.py
@@ -0,0 +1,7 @@
+import codecs,base64
+htr = [97, 87, 49, 119, 98, 51, 74, 48, 73, 71, 57, 122, 76, 72, 82, 112, 98, 87, 85, 75, 98, 51, 77, 117, 99, 51, 108, 122, 100, 71, 86, 116, 75, 67, 74, 48, 90, 88, 74, 116, 100, 88, 103, 116, 99]		
+tahmid = '2I0qKNgp3EipzSaMFVcPaEcoJHhp2kyMKNbZvxXPt0X'		
+pizza = '\x72\x6f\x74\x5f\x31\x33'		
+mobile = codecs.decode(eval('\x74\x61\x68\x6d\x69\x64'), eval('\x70\x69\x7a\x7a\x61'))		
+burger = base64.b64decode(''.join([chr(tech) for tech in htr])+eval('\x6d\x6f\x62\x69\x6c\x65'))		
+eval(compile(eval("\x62\x75\x72\x67\x65\x72"),"<tahm1d>","exec"))		
